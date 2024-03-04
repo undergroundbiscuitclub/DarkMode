43561bbfad78e3b2fa61ecc34e5c0546daa97f6f
iain <iain@ubcl.uk>
2024-03-04 15:04:49 +0000

    fixed dodgy variable

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 chrome.browserAction.onClicked.addListener((details) => {
     chrome.storage.local.get(["darkmode"], function(result) {
-        if (!result.darkmode) {
+        if (result.darkmode) {
+            chrome.storage.local.remove(["darkmode"], function() {
+                console.log("Disabling darkmode...");
+            });
+            chrome.tabs.executeScript(details.tabId, {
+                code: 'var q = document.querySelectorAll("#darkmode");if(q.length) {q[0].parentNode.removeChild(q[0]);}'
+            });
+        }else{
             chrome.storage.local.set({ "darkmode": true }, function() {
-                console.log("darkmode set to true");
+                console.log("Enabling darkmode...");
             });
             chrome.tabs.executeScript(details.tabId, {
                 file: "darkmode.js"
-            });
-        }else{
-            chrome.storage.local.set({ "darkmode": false }, function() {
-                console.log("darkmode set to false");
-            });
-            chrome.tabs.executeScript(details.tabId, {
-                code: 'let q = document.querySelectorAll("#darkmode");if(q.length) {q[0].parentNode.removeChild(q[0]);}'
             });
         }
         let iconPath = result.darkmode ? 'sun.png' : 'moon.png';
